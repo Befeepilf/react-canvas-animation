8cf26fb98210f3a1cd94dd7685043856f574977c
Befeepilf <befeepilf@protonmail.com>
2020-07-25 17:35:37 +0200

    removed unnecessary index files

--- math/src/index.js
+++ math/src/index.js
@@ -1,1 +1,9 @@
-export * from './functions.js';
+export const sin = Math.sin;
+export const cos = Math.cos;
+export const tan = Math.tan;
+export const asin = Math.asin;
+export const acos = Math.acos;
+export const atan = Math.atan;
+export const sqrt = Math.sqrt;
+export const quad = b => Math.pow(b, 2);
+export const pow = Math.pow;
--- vector/src/index.js
+++ vector/src/index.js
@@ -1,1 +1,61 @@
-export {default} from './vector.js';
+import {sin, cos, acos, pow, sqrt} from '@react-canvas-animation/math';
+
+export default class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(v) {
+    this.x += v.x;
+    this.y += v.y;
+    return this;
+  }
+
+  sub(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
+  }
+
+  mult(s) {
+    this.x *= s;
+    this.y *= s;
+    return this;
+  }
+
+  div(s) {
+    this.x /= s;
+    this.y /= s;
+    return this;
+  }
+
+  mag() {
+    return sqrt(pow(this.x, 2) + pow(this.y, 2));
+  }
+
+  setMag(m) {
+    this.div(this.mag()).mult(m);
+    return this;
+  }
+
+  angle() {
+    let a = acos(this.x / this.mag());
+    if(this.y > 0) {
+      a = 2 * Math.PI - a;
+    }
+    return a;
+  }
+
+  rotate(a) {
+    const oldX = this.x;
+    const oldY = this.y;
+    this.x = oldX * cos(a) - oldY * sin(a);
+    this.y = oldX * sin(a) + oldY * cos(a);
+    return this;
+  }
+
+  copy() {
+    return new Vector(this.x, this.y);
+  }
+}
