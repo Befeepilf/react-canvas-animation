d23c0bd19c6671a7a807cc924ca68d3f25cba558
Befeepilf <befeepilf@protonmail.com>
2021-04-18 13:05:06 +0200

    updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const cleaner = require('gulp-clean');
 const spawn = require('child_process').spawn;
 const fs = require('fs');
 
-const version = "0.1.3";
+const version = "0.1.4";
 const entries = ['core', 'draw', 'math', 'vector'];
 
 entries.forEach(entry => {
